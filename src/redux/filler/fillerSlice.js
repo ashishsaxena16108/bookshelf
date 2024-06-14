aa6267843a5d84abf4d0c84dc03ad242a10cff3f
ashish16108 <ashishsaxena2817@gmail.com>
2024-06-14 14:51:59 +0530

    modified:   src/redux/filler/fillerSlice.js

--- src/redux/filler/fillerSlice.js
+++ src/redux/filler/fillerSlice.js
@@ -7,16 +7,17 @@ const fillerSlice = createSlice({
   initialState,
   reducers: {
     addBook(state, book) {
-      state.value?.push(book)
+      state.value.push(book)
       localStorage.setItem("books", JSON.stringify(state.value))
     },
     getBooks(state) {
       const books = JSON.parse(localStorage.getItem("books"))
-      state.value = books
+      if (books !== null)
+        state.value = books
     },
     deleteBook(state, title) {
-      state.value = state.value?.filter((book) => {
-        return book?.payload.title !== title?.payload
+      state.value = state.value.filter((book) => {
+        return book.payload.title !== title.payload
       })
       localStorage.setItem("books", JSON.stringify(state.value))
     }
