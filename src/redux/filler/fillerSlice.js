40ff343bfc9ebcd22392ef7114bb295c9a74b411
ashish16108 <ashishsaxena2817@gmail.com>
2024-06-14 14:29:00 +0530

    modified:   src/redux/filler/fillerSlice.js

--- src/redux/filler/fillerSlice.js
+++ src/redux/filler/fillerSlice.js
@@ -3,25 +3,26 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = { value: [] }
 
 const fillerSlice = createSlice({
-  name:'filler',
+  name: 'filler',
   initialState,
-  reducers:{
-     addBook(state,book){
-         state.value?.push(book)
-         localStorage.setItem("books",JSON.stringify(state.value))
-     },
-     getBooks(state){
-        const books = JSON.parse(localStorage.getItem("books"))
-        state.value=books
-     },
-     deleteBook(state,title){
-       state.value=state.value?.filter((book)=>{
-         return book?.payload.title!==title?.payload
-       })
-       localStorage.setItem("books",JSON.stringify(state.value))
-     }
-  }, 
+  reducers: {
+    addBook(state, book) {
+      state.value?.push(book)
+      if (state.value?.length === 0)
+        localStorage.setItem("books", JSON.stringify(state.value))
+    },
+    getBooks(state) {
+      const books = JSON.parse(localStorage.getItem("books"))
+      state.value = books
+    },
+    deleteBook(state, title) {
+      state.value = state.value?.filter((book) => {
+        return book?.payload.title !== title?.payload
+      })
+      localStorage.setItem("books", JSON.stringify(state.value))
+    }
+  },
 })
 
-export const { addBook,getBooks,deleteBook } = fillerSlice.actions
+export const { addBook, getBooks, deleteBook } = fillerSlice.actions
 export default fillerSlice.reducer
